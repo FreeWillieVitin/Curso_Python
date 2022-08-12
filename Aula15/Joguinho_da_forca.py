dac807d20655c6a0a0f61c75d08318a0afffc498
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2022-08-12 15:33:09 -0300

    Comentado todas as linhas do jogo para entendimento futuro

--- Aula15/Joguinho_da_forca.py
+++ Aula15/Joguinho_da_forca.py
@@ -1,39 +1,43 @@
 # Exemplo de joguinho da forca, neste exemplo é usado uma lista para armazenar as letras existentes na palavra secreta
-# Abaixo estão definidas as variaveis principais do jogo como a palavra secreta, a varivel para armazenar a lista
-# e a variavel da quantidade de chances que o jogador tem.
+# Abaixo estão definidas as variáveis principais do jogo como a palavra secreta, a variável para armazenar a lista
+# e a variável da quantidade de chances que o jogador tem.
 segredo = input("Digite uma palavra: ")
 digitos = []
 chance = 3
 
-while True:
-    if chance == 0:
-        print("Você perdeu!!")
-    letra = input('Digite uma letra: ')
 
-    if len(letra) > 1:
-        print('Letras a mais')
+while True:
+    if chance == 0:  # Se o jogador errar 3 vezes e a variável chance chegar a 0 ele perde e encerra o jogo
+        print(f"Você perdeu!!, A palavra era {segredo}")  # Mostra mensagem da derrota e qual era a palavra correta
+        break
+
+    letra = input('Digite uma letra: ')  # Define a variável que vai pedir a tentativa de letra ao jogador
+    if len(letra) > 1:  # Se o jogador digitar 2 ou mais letra, é checado a quantidade de letras e invalidada a jogada
+        chance = chance - 1  # Desconta uma chance do jogador
+        print(f'Letras a mais, voce perdeu uma chance, agora você tem {chance} chances')  # mostra as chances restantes
         continue
 
-    digitos.append(letra)
-    if letra in segredo:
-        print(f'A letra {letra} bate com a secreta')
-    else:
-        print(f'A letra {letra} não foi encontrada')
-        digitos.pop()
-    temp = ''
-    for l in segredo:
-        if l in digitos:
-            temp += l
-        else:
-            temp += '*'
+    digitos.append(letra)  # Insere a letra digita na lista com a variável de nome digitos
+    if letra in segredo:  # Inicia um laço que verifica se a letra digitada estiver na palavra segredo
+        print(f'A letra {letra} bate com a secreta')  # Se sim, mostra essa mensagem ao jogador
+    else:  # Se não
+        print(f'A letra {letra} não foi encontrada')  # Mostra essa mensagem
+        digitos.pop()  # E exclui o ultimo valor inserido na lista que no caso é a letra errada
 
-    if temp == segredo:
-        print(f'QUe legal, você ganhou!!! A palavra era {temp}')
+    temp = ''  # Variável para armazenar a palavra segredo temporariamente
+    for l in segredo:  # Define a variável l e inicia um laço na variável segredo
+        if l in digitos:
+            temp = temp + l  # Se o valor da variável l for igual a letra da variável digito então ele une os dois
+        else:
+            temp = temp + '*'  # Se a letra nao for igual então recebe o *
+
+    if temp == segredo:  # Verifica se a variável temp tem armazenado a mesma palavra que a secreta, após as tentativas
+        print(f'QUe legal, você ganhou!!! A palavra era {temp}')  # Se o jogador acertou as letras, então parabéns
         break
     else:
-        print(f'Até o momento a palavra está assim: {temp}')
+        print(f'Até o momento a palavra está assim: {temp}')  # Se não forem iguais então o jogo continua e mostra o
+                                                              # andamento do jogo
 
-    if letra not in segredo:
+    if letra not in segredo:  # Se a letra escolhida não estiver na palavra segredo, então tirar uma chance do jogador
         chance = chance - 1
-
     print(f'Você ainda tem {chance} chances')
