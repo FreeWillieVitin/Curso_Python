0c863c5bac7095803bb59996092cc16e9611436d
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2022-08-23 16:01:36 -0300

    Aula de for e else

--- Aula14/For_Else.py
+++ Aula14/For_Else.py
@@ -1,10 +1,19 @@
 """
 For/Else em python
+Iterar = passar por cada valor da lista
 """
 var = ['victor', 'hugo', 'marieli', 'luiz', 'valdirene']
-
+teste = False
+# for i in var:
+#     if i.startswith('v'):  # Função startswith checa se os valores da lista começam com a letra parametrizada
+#         print(i, 'começa com v')
+#     else:
+#         print(i, 'não começa com v')
 for i in var:
     if i.startswith('v'):
-        print(i, 'começa com v')
-    else:
-        print(i, 'não começa com v')
+        teste = True
+
+if teste:
+    print('Existe uma palavra que começa com V')
+else:
+    print('Não existe uma palavra que começa com V')
