55d002aa4803102f0ec72cb117b80a6905d31fc6
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2022-08-29 13:42:09 -0300

    Exercitando um pouco

--- Aula16/SplitJoinEnumerate.py
+++ Aula16/SplitJoinEnumerate.py
@@ -2,5 +2,25 @@
 Funções Split, Join, Enumerate em Python
 * Split - Dividir uma string # str
 * Join - Juntar uma lista # str
-* Enumerate - Enumerar elementos da lista # list
+* Enumerate - Enumerar elementos da lista # list / iteráveis
 """
+string = 'O Brasil é o país do futebol, o Brasil é penta e é tbm muito lindo.'
+lista = string.split(' ')
+lista2 = string.split(',')
+
+palavra = ''
+contagem = 0
+
+for i in lista:
+    print(f'A palavra {i} aparece {lista.count(i)}x na frase.')
+
+for x in lista:
+    qtd_vezes = lista.count(x)
+
+    if qtd_vezes > contagem:
+        contagem = qtd_vezes
+        palavra = x
+
+print(f'A palavra que apareceu mais vezes é {palavra} ({contagem}x)')
+
+print(lista2)
