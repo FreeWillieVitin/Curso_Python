abd827be1c9b7959bb7f392c05427690513eaa2a
Victor <vhjurczyszyn@gmail.com>
2023-01-12 00:11:54 -0300

    condicionais if elif e else

--- Iniciando_Python_Modulo1/Condicionais_Booleans.py
+++ Iniciando_Python_Modulo1/Condicionais_Booleans.py
@@ -5,6 +5,15 @@ elif (else if) = senão se
 else = senão
 Hierarquia = Identação(Espaços entre as linhas para identificar de quem cada codigo pertence)
 """
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não quer nada da vida')
+
 
 if False:
     print('Verdadeiro.')
