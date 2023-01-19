c726f0425138edaba72c39030c6805f73b103b17
Victor <vhjurczyszyn@gmail.com>
2023-01-19 20:31:39 -0300

    manipulaçao, len e exercicio

--- Iniciando_Python_Modulo1/Manipulando_Strings.py
+++ Iniciando_Python_Modulo1/Manipulando_Strings.py
@@ -1,7 +1,7 @@
 """
 Manipulando Strings
-* String indicas
-* Fatiamento de strings
+* String indices
+* Fatiamento de strings [i:f:p] [::]
 * Funções built-in len, abs, type, print, etc
 Essas funções podem ser usadas diretamente em cada tipo.
 """
@@ -10,9 +10,10 @@ texto               ='Python s2'
 print(texto[2])
 # Indices negativos -[987654321]
 url = 'www.google.com/'
-print(url[:-1])
+print(url[:-1]) # Ignora o ultimo caracter pois esta contando de forma negativa
+print(url[::-1]) # Mostra a string invertida
 # Fatiamento
-fat = texto[0:6]
+fat = texto[0:4] # Limita até qual indice o interpretador vai ler
 print(fat)
 # Pular caracteres
 print(texto[0:6:2]) # 0 = começo, 6 = até onde vai, 2 = de quantos em quantos caracteres ele vai pular
