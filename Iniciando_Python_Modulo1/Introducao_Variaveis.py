cf75d547cdf468a6f9c9834420152607bf562a62
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-01-20 15:30:09 -0300

    COnstante

--- Iniciando_Python_Modulo1/Introducao_Variaveis.py
+++ Iniciando_Python_Modulo1/Introducao_Variaveis.py
@@ -39,3 +39,10 @@ CONSTANTE = "Variaveis" que não vai mudar
 Muitas condições no mesmo if (ruim)
     <- Contagem de complexidade (ruim)
 """
+
+velocidade = 61 # Velocidade atual do carro
+local_carro = 101 # local em que o carro está na estrada
+
+RADAR_1 = 60 # Velocidade maxima do radar 1
+LOCAL_1 = 100 # Local onde o radar 1 está
+RADAR_RANGE = 1 # A distancia onde o radar pega
