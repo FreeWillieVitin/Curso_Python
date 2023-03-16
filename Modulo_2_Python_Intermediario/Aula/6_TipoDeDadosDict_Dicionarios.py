8ae1f396fd64a18f36b567bcbed9c7b8d752cab0
Victor <vhjurczyszyn@gmail.com>
2023-03-15 22:08:52 -0300

    Exercicio de perguntas

--- Modulo_2_Python_Intermediario/Aula/6_TipoDeDadosDict_Dicionarios.py
+++ Modulo_2_Python_Intermediario/Aula/6_TipoDeDadosDict_Dicionarios.py
@@ -12,40 +12,40 @@
  Imutáveis: str, int, float, bool, tuple
  Mutável: dict, list
 """
-pessoa = {
+pessoa = { # Está é a forma de criar um dicionario, o primeiro valor é chamado de chavee o seguinte é o valor, separado por :
     'nome': 'Victor Hugo',
     'sobrenome': 'Jurczyszyn',
     'idade': 18,
     'altura': 1.8,
     'endereços': [
         {'rua': 'tal tal', 'número': 123},
-        {'rua': 'outra rua', 'número': 321},
+        {'rua': 'outra rua', 'número': 321}, # Podemos incluir outros dicionarios dentro de um dicionario
     ]
  }
-pessoas = dict(nome='Luiz Otávio', sobrenome='Miranda')
-print(pessoa['nome'])
+pessoas = dict(nome='Luiz Otávio', sobrenome='Miranda') # Outra forma de estrutura de criação de dicionarios, informando a classe dict
+print(pessoa['nome']) # print exibindo apenas uma chave do dicionario
 
-for chave in pessoa:
+for chave in pessoa: # Tambem podemos iterar sobre um dicionario
     print(chave)
     print(chave, pessoa[chave])
 
 # # Manipulando chaves e valores em dicionários
-p = {}
+p = {} # Aqui temos um dicionario sem valores algum
 
-chave = 'nome'
+chave = 'nome' # Variável recebendo o valor nome
 
-p[chave] = 'Victor Hugo'
-p['Sobrenome'] = 'Jurczyszyn'
+p[chave] = 'Victor Hugo' # Ao inserir a variavel mencionada dentro de colchetes referenciando o dicionario seguida por um valor, inserimos uma chave dentro do dicionartio vazio
+p['Sobrenome'] = 'Jurczyszyn' # Faz exatamente a mesma coisa que acima, mostrando que não precisamos obritariamente inserir uma variavel dinamicamente
 
-print(p[chave])
+print(p[chave]) # Exibe apenas a chave e o valor de nome
 
-del p['Sobrenome']
+del p['Sobrenome'] # Deleta a chave descrita do dicionario
 print(p)
 print(p['nome'])
 
-print(p.get('sobrenome', 'Não tem Valor'))
-if p.get('Sobrenome') is None:
-    print('Não existe')
+print(p.get('sobrenome', 'Não tem Valor')) # O metodo get faz uma busca da chave desejada no dicionario, caso não encontre ele retorna um valor padrão de tipo None
+if p.get('Sobrenome') is None: # Aqui fazemos a checagem se a chave é none, ou seja sem valor algum
+    print('Não existe') # Se ela não for encontrada e for definida como None, então damos um retorno padrão
 else:
     print(p['Sobrenome'])
 
@@ -62,25 +62,56 @@ else:
     popitem - Apaga o último item adicionado
     update - Atualiza um dicionário com outro
 """
-print(len(pessoa))
+print(len(pessoa)) # Conta quantas chaves existem no dicionaro
 
-print(list(pessoa.keys()))
-print(tuple(pessoa.keys()))
-for chave in pessoa.keys():
+print(list(pessoa.keys())) # retorna em um tipo lista todas as chaves do dicionario
+print(tuple(pessoa.keys())) # retorna em um tipo tuple todas as chaves do dicionario
+for chave in pessoa.keys(): # itera soomente sobre as chaves
     print(chave)
 
-print(list(pessoa.values()))
-print(tuple(pessoa.values()))
-for valor in pessoa.values():
+print(list(pessoa.values())) # retorna em um tipo lista todas os valores do dicionario
+print(tuple(pessoa.values())) # retorna em um tipo tuple todas os valores do dicionario
+for valor in pessoa.values(): # itera somente sobre os valores
     print(valor)
 
-print(list(pessoa.items()))
-print(tuple(pessoa.items()))
-for chave, valor in pessoa.items():
+print(list(pessoa.items())) # retorna em um tipo lista os items do dicionario que são as chaves e os valores juntos
+print(tuple(pessoa.items())) # retorna em um tipo tuple os items do dicionario que são as chaves e os valores juntos
+for chave, valor in pessoa.items(): # itera sobre os itens, mosntrado de forma mais compreensivel os dados
     print(chave, valor)
 
 print(pessoa.get('Carro', 'Não tem Valor'))
 
-pessoa.setdefault('Planeta', 'Nenhum')
+pessoa.setdefault('Planeta', 'Nenhum') # Para evitar erro, podemos tratar a falta de uma chave dando um valor padrão a ela
 for chave, valor in pessoa.items():
     print(chave, valor)
+
+nome = pessoa.pop('nome') # Apaga uma chave definida como parametro
+print(nome)
+print(pessoa)    
+
+sobrenome = pessoa.popitem() # Apaga a ultima chave do dicionario
+print(sobrenome)
+print(pessoa) 
+
+pessoa.update({  # Atualiza valores ou qualquer dado do dicionario
+    'nome': 'Marieli',
+    'idade': '24'
+})
+
+"""
+Shallow Copy e Deep Copy
+"""
+import copy
+
+d1 = {
+    'c1': 1,
+    'c2': 2,
+    'l1': [0, 1, 2],
+}
+d2 = d1.copy()
+
+d2['c1'] = 1000
+d2['l1'][1] = 999999
+
+print(d1)
+print(d2)
