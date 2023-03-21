380182cecacc13d382d11cbf7dfe244417904ebb
Victor <vhjurczyszyn@gmail.com>
2023-03-20 21:01:20 -0300

    Métodos set e exercicio

--- Modulo_2_Python_Intermediario/Aula/7_Tipo_Set.py
+++ Modulo_2_Python_Intermediario/Aula/7_Tipo_Set.py
@@ -30,3 +30,38 @@ print(l2)
  Métodos úteis:
  add, update, clear, discard
 """
+met = set() # Cria um método vazio
+met.add('Victor') # Adiciona valores ao set
+met.add(1) # Adicionou o valor inteiro 1 ao set
+met.update(('Olá mundo', 1,2,3,4),'teste') # Funciona da mesma forma que o add, esse método atualiza o set, podendo inserir valores
+# met.clear() # Limpa o Set
+met.discard('t') # Elimina valores do set
+print(met)
+
+"""
+ Operadores úteis:
+ | união (union) = Une
+ & (intersection) = Itens presentes em ambos
+ - Diferença = Itens presentes apenas no set da esquerda
+ ^  diferença simétrica = Itens que não estão em ambos
+"""
+op1 = {1, 2, 3} # Criação de 2 sets com valores distintos
+op2 = {2, 3, 4}
+op3 = op1 | op2 # Uma variável que recebe a união dos 2 sets, lembrando que um set nao armazena valores repetidos
+op4 = op1 & op2 # Recebe apenas os valores que estão em ambos os sets, eliminando os valores diferentes
+op5 = op1 ^ op2 # Contrário do anterior, recebe apenas os valores diferentes e elimina os iguais
+op6 = op1 - op2 # Recebe apenas os valores que contém apenas no conjunto da esquerda, varia conforme é contruído
+
+"""
+Exemplo de uso dos sets
+"""
+letras = set()
+while True:
+    letra = input('Digite: ')
+    letras.add(letra.lower())
+
+    if 'l' in letras:
+        print('PARABÉNS')
+        break
+
+    print(letras)
