1c91c6acfe9f5dfd733edfaccb74e179f9a7d566
Victor <vhjurczyszyn@gmail.com>
2023-04-19 21:12:08 -0300

    VARIAVEIS LIVRES E NONLOCAL

--- Modulo_2_Python_Intermediario/Aula/20_Variaveis_livres_nonlocal.py
+++ Modulo_2_Python_Intermediario/Aula/20_Variaveis_livres_nonlocal.py
@@ -1,8 +1,32 @@
 # Variáveis livres + nonlocal
-def fora():
-    a = 1
+def fora(x):
+    a = x
 
     def dentro():
+        print(locals())
+        print(dentro.__code__.co_freevars)
         return a
     return dentro
 
+dentro1 = fora(10)
+dentro2 = fora(20)
+
+print(dentro1())
+print(dentro2())
+print()
+
+def concatenar(si):
+    valor_final = si
+
+    def interna(valor_a_concatenar=''):
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
+final = c()
+print(final)
