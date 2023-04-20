cd2b25d36b78355178367e62a16a5ecc1315ac1f
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-04-20 15:59:36 -0300

    123

---
+++ Modulo_2_Python_Intermediario/Aula/21_Funcoes_Decoradoras.py
@@ -0,0 +1,12 @@
+"""
+    Funções decoradoras e decoradores(DECORATOR)
+    Decorar = Adicionar / Remover/ Restringir / Alterar
+    Funções decoradoras são funções que decoram outras funções
+    Decoradores são usados para fazer o Python
+    usar as funções decoradoras em outras funções.
+"""
+def inverte_string(string):
+    return string[::-1]
+
+invertida = inverte_string('Victor')
+print(invertida)
