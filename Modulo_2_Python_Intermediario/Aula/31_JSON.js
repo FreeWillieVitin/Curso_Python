851d9b186073fcdb27d391d4e830d38e59fcf1b6
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-05-06 11:56:38 -0300

    Como funciona um json em javascript

--- Modulo_2_Python_Intermediario/Aula/31_JSON.js
+++ Modulo_2_Python_Intermediario/Aula/31_JSON.js
@@ -1,6 +1,40 @@
-require('cross-fetch/polyfill')
+// const fs = require('fs');
+// const path = require('path')
+// require('cross-fetch/polyfill');
 
+// pessoas = [
+//     {
+//         nome: 'Victor',
+//         sobrenome: 'Hugo',
+//         idade: 25,
+//         ativo: false,
+//         notas: ['A','A+'],
+//         telefones:{
+//             residencial: '47 3642-2414',
+//             celular: '47 99292-6812',
+//         }
+//     },
+//     {
+//         nome: 'Luiz',
+//         sobrenome: 'Carlos',
+//         idade: 51,
+//         ativo: true,
+//         notas: ['B','A'],
+//         telefones:{
+//             residencial: '47 3642-2414',
+//             celular: '47 98402-7387',
+//         },
+//     },
+// ];
 
+// const saveTo = path.resolve(__dirname, 'arquivo-javascript.json');
 
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(Response => Response.json())
+//   .then(json => {
+//     console.log(json)
+//     fs.writeFileSync(saveTo, JSON.stringify(json, null, 2));
+//   })
 
-fetch('https://jsonplaceholder.typicode.com/posts')
+const json = require('./arquivo-javascript.json')
+json.forEach(post => console.log(post.title))
