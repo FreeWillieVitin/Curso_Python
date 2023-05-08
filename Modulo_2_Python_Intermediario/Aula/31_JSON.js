2182b3ff1f73a5171f8bae30ae52ef61fd6e979c
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-05-08 15:51:27 -0300

    json em python

--- Modulo_2_Python_Intermediario/Aula/31_JSON.js
+++ Modulo_2_Python_Intermediario/Aula/31_JSON.js
@@ -1,40 +1,40 @@
-// const fs = require('fs');
-// const path = require('path')
-// require('cross-fetch/polyfill');
+const fs = require('fs');
+const path = require('path')
+require('cross-fetch/polyfill');
 
-// pessoas = [
-//     {
-//         nome: 'Victor',
-//         sobrenome: 'Hugo',
-//         idade: 25,
-//         ativo: false,
-//         notas: ['A','A+'],
-//         telefones:{
-//             residencial: '47 3642-2414',
-//             celular: '47 99292-6812',
-//         }
-//     },
-//     {
-//         nome: 'Luiz',
-//         sobrenome: 'Carlos',
-//         idade: 51,
-//         ativo: true,
-//         notas: ['B','A'],
-//         telefones:{
-//             residencial: '47 3642-2414',
-//             celular: '47 98402-7387',
-//         },
-//     },
-// ];
+pessoas = [
+    {
+        nome: 'Victor',
+        sobrenome: 'Hugo',
+        idade: 25,
+        ativo: false,
+        notas: ['A','A+'],
+        telefones:{
+            residencial: '47 3642-2414',
+            celular: '47 99292-6812',
+        }
+    },
+    {
+        nome: 'Luiz',
+        sobrenome: 'Carlos',
+        idade: 51,
+        ativo: true,
+        notas: ['B','A'],
+        telefones:{
+            residencial: '47 3642-2414',
+            celular: '47 98402-7387',
+        },
+    },
+];
 
-// const saveTo = path.resolve(__dirname, 'arquivo-javascript.json');
+const saveTo = path.resolve(__dirname, 'arquivo-javascript.json');
 
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//   .then(Response => Response.json())
-//   .then(json => {
-//     console.log(json)
-//     fs.writeFileSync(saveTo, JSON.stringify(json, null, 2));
-//   })
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(Response => Response.json())
+  .then(json => {
+    console.log(json)
+    fs.writeFileSync(saveTo, JSON.stringify(json, null, 2));
+  })
 
 const json = require('./arquivo-javascript.json')
 json.forEach(post => console.log(post.title))
--- Modulo_2_Python_Intermediario/Aula/31_JSON.py
+++ Modulo_2_Python_Intermediario/Aula/31_JSON.py
@@ -11,3 +11,50 @@ null - false
 {} - objeto
 
 """
+import json
+import os
+# pessoas = [
+#     {
+#         "nome": 'Victor',
+#         "sobrenome": 'Hugo',
+#         "idade": 25,
+#         "ativo": False,
+#         "notas": ['A','A+'],
+#         "telefones":{
+#             "residencial": '47 3642-2414',
+#             "celular": '47 99292-6812',
+#         }
+#     },
+#     {
+#         "nome": 'Luiz',
+#         "sobrenome": 'Carlos',
+#         "idade": 51,
+#         "ativo": True,
+#         "notas": ['B','A'],
+#         "telefones":{
+#             "residencial": '47 3642-2414',
+#             "celular": '47 98402-7387',
+#         },
+#     },
+# ]
+
+# BASE_DIR = os.path.dirname(__file__)
+# SAVE_TO = os.path.join(BASE_DIR, 'arquivo-python.json')
+
+# with open(SAVE_TO, 'w+') as file:
+#     json.dump(pessoas, file, indent=2)
+
+# print(json.dumps(pessoas, indent=2))
+# -----------------------------------------------------------------------------------------------------------------------------------
+# Carregar um arquivo json
+
+BASE_DIR = os.path.dirname(__file__)
+JSON_FILE = os.path.join(BASE_DIR, 'arquivo-python.json')
+
+
+with open(JSON_FILE, 'r') as file:
+    arquivos = json.load(file)
+    print(arquivos)
+    print()
+    for arquivo in arquivos:
+       print(arquivo['nome']) 
