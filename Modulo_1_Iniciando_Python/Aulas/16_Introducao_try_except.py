6128cdd9f55817fefed26cfb2c692a3db3b5e211
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-05-26 15:46:51 -0300

    Exceptions em classes e add_note

--- Modulo_1_Iniciando_Python/Aulas/16_Introducao_try_except.py
+++ Modulo_1_Iniciando_Python/Aulas/16_Introducao_try_except.py
@@ -3,63 +3,98 @@ Introdução ao try/except
 try -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
 """
-numero_str = input('Vou dobrar o número que vc digitar: ') # Recebe uma string
+# numero_str = input('Vou dobrar o número que vc digitar: ') # Recebe uma string
 
-try: # tenta executar
-    numero_float = float(numero_str) # COnverte a string em valor float
-    print('FLOAT:', numero_float) # Retorna o valor convertido
-    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}') # Dobra o valor convertido
-except: # Se houver um erro
-    print('Isso não é um número') # Se o valor que esta tentado ser convertido não for um tipo permitido,
-    #  então como excessão, o interpretador retornará essa mensagem
+# try: # tenta executar
+#     numero_float = float(numero_str) # COnverte a string em valor float
+#     print('FLOAT:', numero_float) # Retorna o valor convertido
+#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}') # Dobra o valor convertido
+# except: # Se houver um erro
+#     print('Isso não é um número') # Se o valor que esta tentado ser convertido não for um tipo permitido,
+#     #  então como excessão, o interpretador retornará essa mensagem
 
-# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-"""
-Try, except, else e finally
-"""
-# a = 'a'
-# b = 10
+# # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# """
+# Try, except, else e finally
+# """
+# # a = 'a'
+# # b = 10
 
-# c = a / b
+# # c = a / b
 
+# """
+# Continuando com a tratação de erros, podemos especificar diferentes erros para cada ação incorreta do usuário 
+# """
+
+# try:
+#     a = int(input("Digite um número: "))
+#     b = int(input("Digite um número: ")) # Aqui temos um sistema de divisão, onde obviamente vai dividir o valor de a com o valor de b
+
+#     c = a / b
+#     print(c)
+
+# except ZeroDivisionError as error: # Caso o usuário tente erroneamente realizar uma divisão por 0, o sistema automáticamente mostrará uma mensagem com o erro ZeroDivisionError
+#     print('Impossivel dividir') # Para o usuário é difícil a compreensão do erro se form mostrado isso a ele, dessa maneira se especificarmos após o except qual o erro a ser tratado
+#     print('MSG:', error) # Podemos personalizar a mensagem retornada ao usuário, facilitando a compreensão, isso pode ser feito quantas vezes for necessário, para os diversos tipos de erro
+#     print('Nome:', error.__class__.__name__)
+
+# except NameError:
+#     print('Sem valor definido')
+
+# except ValueError:
+#     print('Sem valor definido')
+
+# except (TypeError, IndexError): # Podemos também mesmo que não sendo correto, tratar mais de um erro no mesmo except, inserindo eles em uma tupla
+#     print('TyperError + IndexError')
+
+# except Exception: # O tipo de erro exception é como se fosse o primeiro da hierarquia dos erros, valendo para todos os outros, caso já tenho sido especificado todos os erros conhecidos
+#     print('Erro Desconhecido') # Mas mesmo assim queira deixar uma tratativa de segurança para algum erro não encontrado, usamos a classe de erro exception
+
+# # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# try:
+#     print('ABRIR O ARQUIVO')
+#     8/0
+
+# except ZeroDivisionError:
+#     print('ERRO')
+
+# else: # O else em uma tratação de erro é uma mensagem se caso não aconteça o erro, pouquissimo usado pois temos a palavra chave finally que faz algo semelhante
+#     print('Não deu erro')
+
+# finally: # O finally ele é uma palavra chave que executará de qualquer forma, tanto se o erro ocorrer, quanto se não ocorrer, imaginamos que nesse exemplo estamos tentando abrir o arquivo
+#     print('FECHAR ARQUIVO') # Para executar alguma coisa, o arquivo abrirá fará o que tem que ser feito e será fechado, se caso aconteça um erro, o arquivo será fechado da mesma forma
+
+# # ---------------------------------------------------------------------------------------------------------------------------------------------
 """
-Continuando com a tratação de erros, podemos especificar diferentes erros para cada ação incorreta do usuário 
+Criando Exceptions em Python Orientado a Objetos
+Para criar uma Exception em Python, você só
+precisa herdar de alguma exceção da linguagem.
+A recomendação da doc é herdar de Exception.
+https://docs.python.org/3/library/exceptions.html
+Criando exceções (comum colocar Error ao final)
+Levantando (raise) / Lançando (throw) exceções
+Relançando exceções
+Adicionando notas em exceções (3.11.0)
 """
+
+class MeuError(Exception): # Classe com excessão, por convenção sempre deve terminar o nome com error e herdar da classe exception
+    ...
+class OutroError(Exception):
+    ...
+
+def levantar():
+    exception_ = MeuError('a', 'b', 'c')
+    exception_.add_note('Nota 1')
+    exception_.add_note('Errou isso')
+    raise exception_
 
 try:
-    a = int(input("Digite um número: "))
-    b = int(input("Digite um número: ")) # Aqui temos um sistema de divisão, onde obviamente vai dividir o valor de a com o valor de b
-
-    c = a / b
-    print(c)
-
-except ZeroDivisionError as error: # Caso o usuário tente erroneamente realizar uma divisão por 0, o sistema automáticamente mostrará uma mensagem com o erro ZeroDivisionError
-    print('Impossivel dividir') # Para o usuário é difícil a compreensão do erro se form mostrado isso a ele, dessa maneira se especificarmos após o except qual o erro a ser tratado
-    print('MSG:', error) # Podemos personalizar a mensagem retornada ao usuário, facilitando a compreensão, isso pode ser feito quantas vezes for necessário, para os diversos tipos de erro
-    print('Nome:', error.__class__.__name__)
-
-except NameError:
-    print('Sem valor definido')
-
-except ValueError:
-    print('Sem valor definido')
-
-except (TypeError, IndexError): # Podemos também mesmo que não sendo correto, tratar mais de um erro no mesmo except, inserindo eles em uma tupla
-    print('TyperError + IndexError')
-
-except Exception: # O tipo de erro exception é como se fosse o primeiro da hierarquia dos erros, valendo para todos os outros, caso já tenho sido especificado todos os erros conhecidos
-    print('Erro Desconhecido') # Mas mesmo assim queira deixar uma tratativa de segurança para algum erro não encontrado, usamos a classe de erro exception
-
-# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-try:
-    print('ABRIR O ARQUIVO')
-    8/0
-
-except ZeroDivisionError:
-    print('ERRO')
-
-else: # O else em uma tratação de erro é uma mensagem se caso não aconteça o erro, pouquissimo usado pois temos a palavra chave finally que faz algo semelhante
-    print('Não deu erro')
-
-finally: # O finally ele é uma palavra chave que executará de qualquer forma, tanto se o erro ocorrer, quanto se não ocorrer, imaginamos que nesse exemplo estamos tentando abrir o arquivo
-    print('FECHAR ARQUIVO') # Para executar alguma coisa, o arquivo abrirá fará o que tem que ser feito e será fechado, se caso aconteça um erro, o arquivo será fechado da mesma forma
+    levantar()
+except (MeuError, ZeroDivisionError) as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    print()
+    exception_ = OutroError('Vou lançar denovo')
+    exception_.add_note('Mais uma nota')
+    exception_.__notes__ += error.__notes__.copy()
+    raise exception_ from error
--- Modulo_3_Introducao_Programacao_Orientada_Objeto/Aulas/13_Polimorfismo.py
+++ Modulo_3_Introducao_Programacao_Orientada_Objeto/Aulas/13_Polimorfismo.py
@@ -15,3 +15,38 @@ por objetos de uma subclasse sem quebrar a aplicação.+
 Sobrecarga de métodos (overload)
 Sobreposição de métodos (override)
 """
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC): # Classe pai que herda método abstrato da classe ABC
+    def __init__(self, mensagem) -> None: # Contrutor com um atributo mensagem
+        self.mensagem = mensagem
+
+    @abstractmethod # Método abstrato do tipo bool, ou seja os seus métodos implementados nas outras classes devem ter um retorno desse mesmo tipo
+    def enviar(self) -> bool:
+        ...    
+
+class NotificacaoEmail(Notificacao): # As duas classes abaixo fazem a mesma coisa, herdam da classe notificação e implementam os seus métodos da classe pai
+    def enviar(self) -> bool:
+        print('E-mail: enviando: ', self.mensagem)
+        return True
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print('SMS: enviando:', self.mensagem)
+        return False
+
+def notificar(notificacao: Notificacao): # Aqui temos o polimorfismo, temos uma função que recebe a classe pai como paramêtro, podendo assim acessar os seus métodos e os métodos dos seus filhos
+    notificacao_enviada = notificacao.enviar() # Para que dessa forma possa realizar suas próprias ações
+    if notificacao_enviada:
+        print('Notificação enviada')
+    else:
+        print('Notificação não enviada')
+
+notificacao_email = NotificacaoEmail('Testando E-Mail')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('Testando SMS')
+notificar(notificacao_sms)
+
+n = NotificacaoSMS('Testando')
+n.enviar()
