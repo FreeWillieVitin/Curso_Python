854232aa1fece91759ec6060e68c4a13e0555c0e
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-06-02 15:58:24 -0300

    metaclasses

--- Modulo_3_Introducao_Programacao_Orientada_Objeto/Aulas/17_Metodo_Call.py
+++ Modulo_3_Introducao_Programacao_Orientada_Objeto/Aulas/17_Metodo_Call.py
@@ -4,12 +4,12 @@ Callable é algo que pode ser executado com parênteses
 Em classes normais, __call__ faz a instância de uma 
 classe "callable"
 """
-class CallMe:
-    def __init__(self, fone) -> None:
+class CallMe: # Função com o método call, com esse método podemos executar o método sem precisar passar parenteses para ele, apenas chamando a função e passando os valores para o atributo
+    def __init__(self, fone) -> None: # Método init onde é criado o atributo fone
         self.fone = fone
 
-    def __call__(self, nome):
+    def __call__(self, nome): # Então criamos o método call e passamos para ele um argumento nome e abaixo a ação do método
         print(nome,'está Chamando,', self.fone)
 
-call1 = CallMe('36422414')
-call1('Judite')
+call1 = CallMe('36422414') # Enfim passamos o valor do atributo fone para um objeto
+call1('Judite') # E passamos o valor do argumento nome do método call sem precisar executar o método diretamente, apenas passando os valores a ação é executada
