6f53787f22ea4784ef6bca7ff05ac7d51a35ee58
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-06-06 15:56:30 -0300

    Exercicio 4 Contrução

---
+++ Modulo_3_Introducao_Programacao_Orientada_Objeto/Exercicios/4_Exercicio_Abstracao_Heranca_Encapsulamento_Polimorfismo/Pessoas.py
@@ -0,0 +1,60 @@
+"""
+Exercício com Abstração, Herança, Encapsulamento e Polimorfismo
+Criar um sistema bancário (extremamente simples) que tem clientes, contas e
+um banco. A ideia é que o cliente tenha uma conta (poupança ou corrente) e que
+possa sacar/depositar nessa conta. Contas corrente tem um limite extra.
+Conta (ABC)
+    ContaCorrente
+    ContaPoupanca
+Pessoa (ABC)
+    Cliente
+        Clente -> Conta
+Banco
+    Banco -> Cliente
+    Banco -> Conta
+Dicas:
+Criar classe Cliente que herda da classe Pessoa (Herança)
+    Pessoa tem nome e idade (com getters)
+    Cliente TEM conta (Agregação da classe ContaCorrente ou ContaPoupanca)
+Criar classes ContaPoupanca e ContaCorrente que herdam de Conta
+    ContaCorrente deve ter um limite extra
+    Contas têm agência, número da conta e saldo
+    Contas devem ter método para depósito
+    Conta (super classe) deve ter o método sacar abstrato (Abstração e
+    polimorfismo - as subclasses que implementam o método sacar)
+Criar classe Banco para AGREGAR classes de clientes e de contas (Agregação)
+Banco será responsável autenticar o cliente e as contas da seguinte maneira:
+    Banco tem contas e clentes (Agregação)
+    * Checar se a agência é daquele banco
+    * Checar se o cliente é daquele banco
+    * Checar se a conta é daquele banco
+Só será possível sacar se passar na autenticação do banco (descrita acima)
+Banco 
+"""
+from abc import ABC, abstractmethod
+from conta import ContaPoupanca, ContaCorrente, Conta
+
+class Pessoa(ABC):
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, n):
+        self._nome = n
+
+    @property
+    def idade(self):
+        return self._idade
+    
+    @idade.setter
+    def idade(self, i):
+        self._idade = i
+
+class Cliente(Pessoa):
+    def __init__(self, conta):
+        self.conta = conta
