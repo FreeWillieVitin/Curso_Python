31e4b500ca6d33b9983a0d6740d2bf1c616955c6
Victor <vhjurczyszyn@gmail.com>
2023-06-22 20:34:55 -0300

    Modulo Calendar e locale

--- Modulo_4_Modulos_em_Python/Aula/Aula3_Locale.py
+++ Modulo_4_Modulos_em_Python/Aula/Aula3_Locale.py
@@ -2,6 +2,7 @@
 Locale para internacionalização
 https://docs.python.org/3/library/locale.html
 https://learn.microsoft.com/fr-fr/powershell/module/international/get-winsystemlocale?view=windowsserver2022-ps&viewFallbackFrom=win10-ps
+Get-WinSystemLocale - Ver o locale do sistema operacional
 """
 import calendar
 import locale
@@ -9,3 +10,4 @@ import locale
 locale.setlocale(locale.LC_ALL, '')
 
 print(calendar.calendar(2023))
+print(locale.getlocale())
