45f1ffb65a0bbc05b245c278963dd8d9af84559d
Victor <vhjurczyszyn@gmail.com>
2023-06-22 20:39:51 -0300

    locale linux

--- Modulo_4_Modulos_em_Python/Aula/Aula3_Locale.py
+++ Modulo_4_Modulos_em_Python/Aula/Aula3_Locale.py
@@ -3,6 +3,7 @@ Locale para internacionalização
 https://docs.python.org/3/library/locale.html
 https://learn.microsoft.com/fr-fr/powershell/module/international/get-winsystemlocale?view=windowsserver2022-ps&viewFallbackFrom=win10-ps
 Get-WinSystemLocale - Ver o locale do sistema operacional
+locale -a no linux - Mostra todos as localizações 
 """
 import calendar
 import locale
