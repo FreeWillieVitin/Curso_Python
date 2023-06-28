669e1ee7b89594418037ea5f8aa4e93745fa2b54
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-06-28 15:13:34 -0300

    comentando o modulo os

--- Modulo_4_Modulos_em_Python/Aula/Aula4_Modulo_OS.py
+++ Modulo_4_Modulos_em_Python/Aula/Aula4_Modulo_OS.py
@@ -67,13 +67,13 @@ C:\\Users\\Victor\\Desktop\\imagens
 caminho2 = ('C:\\Users\\Victor\\Desktop\\imagens') # Variável armazenando o caminho completo do arquivo
 
 for pasta in os.listdir(caminho2): # A função listdir é usada para listar tudo o que está dentro do arquivo, seus documentos, subpastas, imagens e etc
-    caminho_completo = os.path.join(caminho2, pasta) # É uma função iteravel, o python itera sobre o caminho e retorna item por item
+    caminho_completo = os.path.join(caminho2, pasta) # É uma função iteravel, o python itera sobre o caminho e retorna item por item, nesta linha a variável caminho_completo está concatenando o o caminho passado com o nome item iterado
     print(pasta)
 
-    if not os.path.isdir(caminho_completo):
+    if not os.path.isdir(caminho_completo): # A função isdir checa se caminho atual é um diretório ou não, no caso do código, se não for um diretório, se for uma imagem ou qualquer arquivo q não seja uma pasta, então o python irá retornar true, caso contrário false
         continue
     
-    for imagem in os.listdir(caminho_completo):
+    for imagem in os.listdir(caminho_completo): # E para o que for subpastas iteramos sobre o que está la dentro também
         print('  ', imagem)
 print()
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -84,22 +84,22 @@ os.walk é uma função que permite percorrer uma estrutura de diretórios de ma
 Ela gera uma sequencia de tuplas, onde cada tupla possui três elementos: O diretório atual (root), uma lista de subdiretórios (dirs)
 e uma lista dos arquivos do diretório atual (files).
 """
-import os
-from itertools import count
+import os # Importando o módulo os
+from itertools import count # Importando a classe count do módulo itertools
 
-caminho3 = os.path.join('\\Users', 'Victor', 'Desktop', 'Xpadder 5.7 JogoGames Br (Atualizado)')
-counter = count()
+caminho3 = os.path.join('\\Users', 'Victor', 'Desktop', 'Xpadder 5.7 JogoGames Br (Atualizado)') # Definindo um diretório
+counter = count() # A Classe count, como o nome já diz, cria um contador que a cada iteração é somado 1 em seu valor
 
-for root, dirs, files in os.walk(caminho3):
-    the_counter = next(counter)
-    print(the_counter, 'Root:', root)
+for root, dirs, files in os.walk(caminho3): # Para iterar em todas as partes de um diretorio, definimos 3 variáveis, uma para a raiz, outra para os diretórios dentro da raiz e a ultima para os arquivos em cada diretório
+    the_counter = next(counter) # E apenas para identificar, criamos uma variável que vai receber a contagem de itens a cada iteração
+    print(the_counter, 'Root:', root) # Exibe a contagem e o caminho raíz
 
-    for dir_ in dirs:
+    for dir_ in dirs: # Itera os diretórios presentes na raiz
         print(the_counter, 'Dir', dir_)
 
-    for file_ in files:
+    for file_ in files: # Itera sobre os arquivos, separados por diretórios
         print(the_counter, 'Files', file_)
-        caminho_completo2 = os.path.join(root, file_)
+        caminho_completo2 = os.path.join(root, file_) # Variável apenas para unir todas as informações e mostrar o caminho completo de cada arquivo
         print('  ', the_counter, 'FILE:', caminho_completo2)
         # os.unlink(caminho_completo2) # Exclui todos os arquivos que estão nesse caminho NÃO EXECUTAR DE JEITO NENHUM PELO AMOR DE DEUS
 print()
@@ -110,6 +110,7 @@ os.path.getsize e os.stat para dados dos arquivos
 """
 import math
 
+# Abaixo temos uma função para exibir de forma mais apresentável o tamanho de cada arquivo, de forma que mostre o tamanho e a sua unidade de medida
 def formata_tamanho(tamanho_em_bytes: int, base: int = 1024) -> str:
     """Formata um tamanho, de bytes para o tamanho apropriado"""
 
@@ -145,13 +146,13 @@ for root, dirs, files in os.walk(caminho4):
 
     for dir_ in dirs:
         caminho_completo4 = os.path.join(root, dir_)
-        stats = os.stat(caminho_completo4)
-        tamanho = stats.st_size
+        stats = os.stat(caminho_completo4) # os.stats retorna todas as informações sobre um arquivo ou diretório
+        tamanho = stats.st_size # E a função stats tem propriedades para especificar quais informações sobre o arquivo ou diretório for desejada, no exemplo do codigo temos a propriedade st_size que retorna o tamanho do arquivo
         print(the_counter, 'Dir', dir_, formata_tamanho(tamanho))
 
     for file_ in files:
         caminho_completo3 = os.path.join(root, file_)
-        tamanho1 = os.path.getsize(caminho_completo3)
+        tamanho1 = os.path.getsize(caminho_completo3) # Função getsize também retorna o tamanho do arquivo porém sem formatação, apenas o valor concreto
         print('  ', the_counter, 'FILE:', caminho_completo3, formata_tamanho(tamanho1))
 print()
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -169,25 +170,25 @@ echo $HOME
 import os
 import shutil
 
-HOME = os.path.expanduser('~')
-DESKTOP = os.path.join(HOME, 'Desktop')
-PASTA_ORIGINAL = os.path.join(DESKTOP, 'imagens')
-NOVA_PASTA = os.path.join(DESKTOP, 'AULA')
+HOME = os.path.expanduser('~') # Está constante recebe o caminho completo do diretório atraves da função expanduser(), o '~' representa o diretório home do usuário, podendo variar de sistema operacional
+DESKTOP = os.path.join(HOME, 'Desktop') # Assim construimos um diretório, juntando a raiz com o desktop
+PASTA_ORIGINAL = os.path.join(DESKTOP, 'imagens') # O desktop com a pasta imagens
+NOVA_PASTA = os.path.join(DESKTOP, 'AULA') # E criamos tbm um caminho para onde uma nova pasta pode ser criada, no caso a pasta aula será criada no diretório que representa a constante DESKTOP
 
-# os.makedirs(NOVA_PASTA, exist_ok=True)
+# os.makedirs(NOVA_PASTA, exist_ok=True) # A função makedirs cria pastas no diretoório definido como primeiro argumento da função, também podemos checar se ja existe a pasta definindo o parâmetro exist_ok = True
 
-# for root, dirs, files in os.walk(PASTA_ORIGINAL):
-#     for dir_ in dirs:
-#         caminho_novo_diretorio = os.path.join(root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_)
-#         os.makedirs(caminho_novo_diretorio, exist_ok=True)
+# for root, dirs, files in os.walk(PASTA_ORIGINAL): # Com a pasta criada iteramos sobre os arquivos da pasta original
+#     for dir_ in dirs: # Passando também pelas subpastas
+#         caminho_novo_diretorio = os.path.join(root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_) # Salvando uma variável com a junção de cada subpasta
+#         os.makedirs(caminho_novo_diretorio, exist_ok=True) # E desta forma criamos cada subpasta usando a função makedir e passando a variável com o caminho novo que incluí as subpastas
 
-#     for file in files:
-#         caminho_original = os.path.join(root, file)
-#         caminho_nova_pasta = os.path.join(root.replace(PASTA_ORIGINAL, NOVA_PASTA), file)
-#         shutil.copy(caminho_original, caminho_nova_pasta)
+#     for file in files: # Iteramos também sobre os arquivos
+#         caminho_original = os.path.join(root, file) # Passando o caminho da pasta original para variável e percorremos as variáveis root(raíz) e file(arquivos)
+#         caminho_nova_pasta = os.path.join(root.replace(PASTA_ORIGINAL, NOVA_PASTA), file) # Fazendo a mesma coisa com as subpastas
+#         shutil.copy(caminho_original, caminho_nova_pasta) # E usando a funcção copy do modulo shutil copiamos tudo os que está de uma pasta para a outra
 #         print(caminho_nova_pasta)
 
-# shutil.rmtree(NOVA_PASTA, ignore_errors=True)
+# shutil.rmtree(NOVA_PASTA, ignore_errors=True) # A função rmtree excluí pastas DE FORMA DEFINITIVA, AO EXECUTAR ESSA FUNÇÃO OS ARQUIVOS NÃO SERÃO ENVIÁDOS A LIXEIRA
 # os.unlink(NOVA_PASTA)
-# shutil.copytree(PASTA_ORIGINAL, NOVA_PASTA)
-shutil.move(NOVA_PASTA, NOVA_PASTA + '_EU')
+# shutil.copytree(PASTA_ORIGINAL, NOVA_PASTA) # Aqui temos a forma simples de copiar arquivos de uma pasta para a outra, a forma anterior é mais para enender como o python realiza esse tipo de operação
+shutil.move(NOVA_PASTA, NOVA_PASTA + '_EU') # E a função move renomeia ou move um arquivo de lugar ação essa que para o python é a mesma coisa, ja que estamos alterando a propriedade do diretório de um arquivo ao fazer isso
