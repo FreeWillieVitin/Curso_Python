e1e7fecbd6583a44e48a47bf425748c147afa8a1
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-07-04 15:59:41 -0300

    Introdução CSV

---
+++ Modulo_4_Modulos_em_Python/Aula/Aula6_CSV.py
@@ -0,0 +1,11 @@
+"""
+CSV (Comma Separated Values - Valores separados por vírgulas)
+É um formato de arquivo que armazena dados em forma de tabela, onde cada
+linha representa uma linha da tabela e as colunas são separadas por vírgulas.
+Ele é amplamente utilizado para transferir dados entre sistemas de diferentes
+plataformas, como por exemplo, para importar ou exportar dados para uma
+planilha (Google Sheets, Excel, LibreOffice Calc) ou para uma base de dados.
+Um arquivo CSV geralmente tem a extensão ".csv" e pode ser aberto em um
+editor de texto ou em uma planilha eletrônica.
+Um exemplo de um arquivo CSV pode ser:
+"""
