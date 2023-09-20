be10f7b4143440e3858fa0dbd7f3272e96fe7cca
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-09-20 15:58:18 -0300

    Layout e adicionar itens no form

--- Modulo_4_Modulos_em_Python/Aula/Aula9_StringTemplate.py
+++ Modulo_4_Modulos_em_Python/Aula/Aula9_StringTemplate.py
@@ -13,18 +13,18 @@ import string
 from datetime import datetime
 from pathlib import Path
 
-CAMINHO_ARQUIVO = Path(__file__).parent / 'Aula9_Texto_teste.txt'
+CAMINHO_ARQUIVO = Path(__file__).parent / 'Aula9_Texto_teste.txt'  # variável que armazena o caminho de um arquivo de texto
 
 locale.setlocale(locale.LC_ALL, '')
 
 
-def converte_para_brl(numero: float | int) -> str:
+def converte_para_brl(numero: float | int) -> str:  # Função que usa o localização atual e converte simbolos monetários
     brl = locale.currency(numero, symbol=True, grouping=True)
     return brl
 
 
-data = datetime(2022, 10, 28)
-dados = dict(
+data = datetime(2022, 10, 28)  # Variável com uma data pré-estabelecida
+dados = dict(  # Dicionário de onde os valores serão repassados para o arquivo txt
     nome='Victor',
     valor=converte_para_brl(1_234_456),
     data=data.strftime('%d/%m/%Y'),
@@ -32,13 +32,16 @@ dados = dict(
     telefone='+55 (47) 3642-2479'
 )
 
-print(json.dumps(dados, indent=2, ensure_ascii=False))
+print(json.dumps(dados, indent=2, ensure_ascii=False))  # Converte as informações do dicionário para JSON
 print()
 
+# O texto abaixo, é um texto normal que será passado para o arquivo, as informações que vão receber os dados do dicionário ficam
+# após o símbolo $ e seguido de colchetes, dentro dos colchetes informamos a chave da informação que será passada da mesma forma
+# que está escrito no dicionário, ou podemos usar apenas o símbolo de cifrão $ seguido da chave.
 texto = """
 Prezado(a) $nome,
 
-Informamos que sua mensalidade será cobrada no valor de ${valor} no dia $data. Caso deseje cancelar o serviço, entre em contato 
+Informamos que sua mensalidade será cobrada no valor de ${valor} no dia $data. Caso deseje cancelar o serviço, entre em contato
 com a $empresa pelo telefone $telefone.
 
 Atenciosamente,
@@ -46,13 +49,15 @@ Atenciosamente,
 ${empresa},
 Abraços
 """
-template = string.Template(texto)
-print(template.substitute(dados))
+template = string.Template(texto)  # Variável que recebe o template do texto acima, dando assim a posibilidade de substituição
+print(template.substitute(dados))  # A função substitute troca os colchetes com chaves pelos dados do dicionário
 print()
 
-# class MyTemplate(string.Template): # Alterar o delimitador do texto, não aconselhavel mas se for usar nao esquecer de aplica-lo no código de leitura do arquivo
-#     delimiter = '%'
+# class MyTemplate(string.Template): # Alterar o delimitador do texto, não aconselhavel mas se for usar nao esquecer de aplica-lo
+#     delimiter = '%'                # no código de leitura do arquivo
 
+# Também podemos ler um arquivo pronto e somente realizar as substituiçõe, dessa forma precisamos criar o arquivo de texto em
+# txt ou qualquer escrito de texto nos mesmo padrões acima e executar as substituições
 with open(CAMINHO_ARQUIVO, 'r', encoding='utf8') as arquivo:
     texto = arquivo.read()
     template = string.Template(texto)
--- Modulo_5_InterfaceGrafica_PySide6/Aula/Aula2_QApplication.py
+++ Modulo_5_InterfaceGrafica_PySide6/Aula/Aula2_QApplication.py
@@ -4,17 +4,53 @@ QApplication -> O Widget principal da aplicação
 QPushButton -> Um botão
 PySide6.QtWidgets -> Onde estão os widgets do PySide6
 """
+# import sys
+
+# from PySide6.QtWidgets import QApplication, QPushButton
+
+# app = QApplication()
+
+# botao = QPushButton('Marieli Amor da Minha Vida')  # Exibe um botão na tela com um texto dentro
+# botao.setStyleSheet('font-size: 40px; color: Pink; background-color: blue')  # Estiliza o botão usando comandos CSS
+# botao.show()  # Adiciona o widget na hierarquia e exibe a janela
+
+# botao2 = QPushButton('Botão2')
+# botao2.show()
+
+# app.exec()  # Executa a aplicação
+# --------------------------------------------------------------------------------------------------------------------------------
+
+"""
+# QWidget e QLayout de PySide6.QtWidgets
+# QWidget -> genérico
+# QLayout -> Um widget de layout que recebe outros widgets
+
+Os processos da aplicação - QMainWindow e centralWidget
+-> QApplication
+"""
 import sys
 
-from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtWidgets import QApplication, QPushButton, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout
 
-app = QApplication(sys.argv)
+app1 = QApplication()  # # Indica um inicializador de aplicação para a variável
 
-botao = QPushButton('Marieli Amor da Minha Vida')
-botao.setStyleSheet('font-size: 40px; color: Pink; background-color: blue')
-botao.show()  # Adiciona o widget na hierarquia e exibe a janela
+botao3 = QPushButton('Marieli Amor da Minha Vida')
+botao3.setStyleSheet('font-size: 40px; color: Pink; background-color: blue')
 
-botao2 = QPushButton('Botão2')
-botao2.show()
+botao4 = QPushButton('Botão')
+botao4.setStyleSheet('font-size: 20px; color: Pink; background-color: blue')
 
-app.exec()  # Executa a aplicação
+botao5 = QPushButton('Botão2')
+botao5.setStyleSheet('font-size: 20px; color: Pink; background-color: blue')
+
+central_widget = QWidget()  # O QWidget seria como um formulário onde é posicionado os itens e exibido ao usuário
+
+layout = QGridLayout()  # QGridLayout é um layout que seria como uma tabela, existem vários outros tipos e layout
+central_widget.setLayout(layout)  # Define qual o tipo de layout da determinada sessão da aplicação
+
+layout.addWidget(botao3, 1, 2, 1, 1)  # A função addWidget adiciona item em um layout, essa função também recebe valores
+layout.addWidget(botao4, 1, 1, 1, 1)  # de posicionamento, após indicar o que será adicionado, o proximo parâmetro indica em qual
+layout.addWidget(botao5, 3, 1, 1, 2)  # linha o item se posicionará, a seguir a coluna
+
+central_widget.show()  # Central widget entre na hierarquia e mostre sua janela
+app1.exec()
