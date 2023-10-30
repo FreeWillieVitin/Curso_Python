9f00c37589e8552870a37f168530eda9709aa78a
Victor Hugo Jurczyszyn <76778540+FreeWillieVitin@users.noreply.github.com>
2023-10-30 15:29:55 -0300

    PDF

--- PDF_Editor.py
+++ PDF_Editor.py
@@ -1,17 +1,23 @@
 import PyPDF2
 
 with open('C:\\Users\\HSVP\\Desktop\\Escala Final de Semana.pdf', 'rb') as arquivo:  # Abre o arquivo em modo de leitura byte(rb)
-    leitor = PyPDF2.PdfReader(arquivo)  # Classe de leitura que recebe a variável do arquivo aberto
-
-    qtd_pages = len(leitor.pages)  # Faz a contagem de páginas
-
-    num_pages = 0  # Indíce da página 0 = primeira página
-    pagina = leitor.pages[num_pages]  # Armazena a estrutura abstrata da página passada como índice
-    page_text = pagina.extract_text()
+    pdf_reader = PyPDF2.PdfReader(arquivo)  # Classe de leitura que recebe a variável do arquivo aberto
+    page_number = len(pdf_reader.pages)  # Faz a contagem de páginas
+    page_number = 0  # Indíce da página 0 = primeira página
+    page = pdf_reader.pages[page_number]  # Armazena a estrutura abstrata da página passada como índice
+    page_text = page.extract_text()
 
     linhas = page_text.split('\n')
+    linhas[9] = 'É nois Brow'
+    novo_pdf = '\n'.join(linhas)
 
-    # linhas['VICTOR HUGO JURCZYSZYN  Informática  08:00h às 12:00h  '] = 'VICTOR HUGO JURCZYSZYN  Informática  10:00h às 12:00h'
+    # escrever_pdf = PyPDF2.PdfWriter()
 
+    # for i, pages in enumerate(pdf_reader.pages):
+    #     escrever_pdf = PyPDF2.PdfWriter()
+    #     with open('C:\\Users\\HSVP\\Desktop\\NovoPdf.pdf', 'wb') as new_file:
+    #         # Escreve o conteúdo modificado no novo arquivo PDF
+    #         escrever_pdf.add_page(pages)
+    #         escrever_pdf.write(new_file)
 
 print(linhas)
