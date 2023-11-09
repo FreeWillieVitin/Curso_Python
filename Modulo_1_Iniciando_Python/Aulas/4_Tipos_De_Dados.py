fd608526e5348834502f8672ec0ea03cee40f428
Victor Hugo Jurczyszyn <>
2023-11-09 16:01:35 -0300

    orginanzondo documentação

--- Modulo_1_Iniciando_Python/Aulas/4_Tipos_De_Dados.py
+++ Modulo_1_Iniciando_Python/Aulas/4_Tipos_De_Dados.py
@@ -12,6 +12,7 @@ ctrl + ;
 Tudo em python é um objeto
 """
 
+from math import isclose
 print('Victor', type('Victor'))  # type mostra o tipo de dado da classe.
 print(-10, type(-10))
 print(20.50, type(20.50))
@@ -36,6 +37,7 @@ https://docs.python.org/pt-br/3/tutorial/floatingpoint.html
 Existem casos que calculos com numeros flutuantes não retornam o valor de forma precisa, então nesses raros casos
 temos algumas formas de corrigir estas imprecisões
 """
+
 num1 = 0.1
 num2 = 0.7
 num3 = num1 + num2
@@ -48,4 +50,6 @@ print(f'{num3:.2f}')
 
 # Outra forma de tratar esse erro é usando a função round, porém com essa função o numero será totalmente arredondado e nessa
 # caso retorna = 0.8
-print(round(num3, 2))
+print(round(num3, ndigits=1))
+
+print(isclose(0.1 + 0.7, 1.5, abs_tol=0.8))
