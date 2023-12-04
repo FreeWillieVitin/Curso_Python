22c8751c111931cdc6ec480e08acb1ca01bca98f
Victor Hugo Jurczyszyn <>
2023-12-04 15:49:35 -0300

    conhecendo o docker

--- Modulo_6_Bases_de_Dados/Aulas/Aula3_Docker_Container_MySQL_WSL2.py
+++ Modulo_6_Bases_de_Dados/Aulas/Aula3_Docker_Container_MySQL_WSL2.py
@@ -1,5 +1,6 @@
 """
 O que são containers: https://www.youtube.com/watch?v=85k8se4Zo70
 WSL2 e Docker no Windows: https://www.youtube.com/watch?v=05YN8F8ajBc
+Documentação de instalação do WSL2: https://docs.microsoft.com/en-us/windows/wsl/install-win10
 Documentação de instalação Docker: https://docs.docker.com/engine/install/ubuntu/
 """
