75d0f4e3ea2b6da90c3bbd3719bdae58dde88fc6
Victor Hugo Jurczyszyn <>
2023-12-15 16:01:33 -0300

    Padrões de projeto

--- Modulo_3_Introducao_Programacao_Orientada_Objeto/Aulas/12_Classes_Abstratas.py
+++ Modulo_3_Introducao_Programacao_Orientada_Objeto/Aulas/12_Classes_Abstratas.py
@@ -21,23 +21,30 @@ from abc import ABC, ABCMeta, abstractmethod
 # class Log(metaclass=ABCMeta):
 #     pass
 
-class Log(ABC): # A classe que possuir um método abstrado deve herdar a função de abstração da classe ABC
-    @abstractmethod # Decorator que indica que o método é um método abstrato
-    def _log(self, msg): ... # Método Abstrato não tem sua execução partida da classe da qual ela pertence, a classe abstrata precisa ser implementada por outra classe filha, usando os mesmo padrão de contruçao do método da classe pai
-    
-    def log_error(self, msg): # Método Concreto pode ser executado diretamente da classe em que ele foi criado
+
+class Log(ABC):  # A classe que possuir um método abstrado deve herdar a função de abstração da classe ABC
+    @abstractmethod  # Decorator que indica que o método é um método abstrato
+    # Método Abstrato não tem sua execução partida da classe da qual ela pertence, a classe abstrata precisa ser implementada por
+    # outra classe filha, usando os mesmo padrão de contruçao do método da classe pai
+    def _log(self, msg):
+        ...
+
+    def log_error(self, msg):  # Método Concreto pode ser executado diretamente da classe em que ele foi criado
         return self._log(f'Error: {msg}')
-    
+
     def log_success(self, msg):
         return self._log(f'Success: {msg}')
-    
+
+
 class LogPrintMixin(Log):
     def _log(self, msg):
         print(f'{msg} ({self.__class__.__name__})')
 
+
 l = LogPrintMixin()
 l.log_error('Oi')
-# -----------------------------------------------------------------------------------------------------------------------------------------
+print()
+# --------------------------------------------------------------------------------------------------------------------------------
 
 """
 abstractmethod para qualquer método já decorado (@property e setter)
@@ -47,19 +54,28 @@ use @abstractmethod como decorator mais interno.
 Foo - Bar são palavras usadas como placeholder
 para palavras que podem mudar na programação.
 """
-print()
-class AbstractFoo(ABC): # Classe pai que herda métodos e decoradores da classe ABC responsável pela criação de métodos abstratos
-    def __init__(self, name): # Método inicializador
-        self._name = None # Criamos primeiramente o atributo protegida _name que é o atributo receptor do valor a ser setado para a property
-        self.name = name # O setter passar o valor para a property name para que o valor possa ser retornado
-        
+
+
+class AbstractFoo(ABC):  # Classe pai que herda métodos e decoradores da classe ABC responsável pela criação de métodos abstratos
+    def __init__(self, name):  # Método inicializador
+        self._name = None  # Criamos primeiro o atributo _name que é o atributo receptor do valor a ser setado na property
+        self.name = name  # O setter passar o valor para a property name para que o valor possa ser retornado
+
+    # Criação de uma property abstrata, para que a property possa ser abstrata o decorator @abstractmethod precisa estar o mais
+    # perto possivel da assinatura do method
     @property
     @abstractmethod
-    def name(self): ... # Criação de uma property abstrata, para que a property possa ser abstrata o decorator @abstractmethod precisa estar o mais perto possivel da assinatura do method
+    def name(self):
+        ...
 
-    @name.setter # Por haver um property abstrato, precisamos ter uma assinatura de um setter para passar o valor, que é definito não na classe pai quanto na classe filha
-    # @abstractmethod O setter também pode ser abstrato e para isso passamos o decorator da mesma forma que na property e para chama-lo nma classe filha usamos o decorator da seguinte forma @NomeDaClassePai.nomedometodo.setter
-    def name(self, name): ... #Enfim é passado a assinatura do método setter
+    # Por haver um property abstrato, precisamos ter uma assinatura de um setter para passar o valor, que é definito tanto na
+    # classe pai quanto na classe filha
+    @name.setter
+    # @abstractmethod O setter também pode ser abstrato e para isso passamos o decorator da mesma forma que na property e para
+    # chama-lo nma classe filha usamos o decorator da seguinte forma @NomeDaClassePai.nomedometodo.setter
+    def name(self, name):  # Enfim é passado a assinatura do método setter
+        ...
+
 
 class Foo(AbstractFoo):
     # name = ''
@@ -69,13 +85,14 @@ class Foo(AbstractFoo):
         # print('Sou Inútil')
 
     @property
-    def name(self): 
+    def name(self):
         return self._name
 
     # @AbstractFoo.name.setter
     @name.setter
-    def name(self, name): 
+    def name(self, name):
         self._name = name
+
 
 foo = Foo('Bar')
 print(foo.name)
--- Modulo_3_Introducao_Programacao_Orientada_Objeto/Aulas/13_Polimorfismo.py
+++ Modulo_3_Introducao_Programacao_Orientada_Objeto/Aulas/13_Polimorfismo.py
@@ -17,30 +17,38 @@ Sobreposição de métodos (override)
 """
 from abc import ABC, abstractmethod
 
-class Notificacao(ABC): # Classe pai que herda método abstrato da classe ABC
-    def __init__(self, mensagem) -> None: # Contrutor com um atributo mensagem
+
+class Notificacao(ABC):  # Classe pai que herda método abstrato da classe ABC
+    def __init__(self, mensagem) -> None:  # Contrutor com um atributo mensagem
         self.mensagem = mensagem
 
-    @abstractmethod # Método abstrato do tipo bool, ou seja os seus métodos implementados nas outras classes devem ter um retorno desse mesmo tipo
+    # Método abstrato do tipo bool, ou seja os seus métodos implementados nas outras classes devem ter um retorno desse mesmo tipo
+    @abstractmethod
     def enviar(self) -> bool:
-        ...    
+        ...
 
-class NotificacaoEmail(Notificacao): # As duas classes abaixo fazem a mesma coisa, herdam da classe notificação e implementam os seus métodos da classe pai
+
+# As duas classes abaixo fazem a mesma coisa, herdam da classe notificação e implementam os seus métodos da classe pai
+class NotificacaoEmail(Notificacao):
     def enviar(self) -> bool:
         print('E-mail: enviando: ', self.mensagem)
         return True
+
 
 class NotificacaoSMS(Notificacao):
     def enviar(self) -> bool:
         print('SMS: enviando:', self.mensagem)
         return False
 
-def notificar(notificacao: Notificacao): # Aqui temos o polimorfismo, temos uma função que recebe a classe pai como paramêtro, podendo assim acessar os seus métodos e os métodos dos seus filhos
-    notificacao_enviada = notificacao.enviar() # Para que dessa forma possa realizar suas próprias ações
+
+# O polimorfismo, é uma função que recebe a classe pai como paramêtro, e acessa os seus métodos e os métodos dos seus filhos
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()  # Para que dessa forma possa realizar suas próprias ações
     if notificacao_enviada:
         print('Notificação enviada')
     else:
         print('Notificação não enviada')
+
 
 notificacao_email = NotificacaoEmail('Testando E-Mail')
 notificar(notificacao_email)
