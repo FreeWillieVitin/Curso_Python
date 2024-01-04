8f8b1df1fada6c2cb72f26df972a24184302f1d7
Victor Hugo Jurczyszyn <>
2024-01-04 15:55:04 -0300

    Factory method comentada

--- Modulo_Extra_Padroes_de_Projeto/Aula/Aula3_Factory_Method.py
+++ Modulo_Extra_Padroes_de_Projeto/Aula/Aula3_Factory_Method.py
@@ -6,12 +6,12 @@ O Factory method permite adiar a instanciação para as subclasses, garantindo o
 from abc import ABC, abstractmethod
 
 
-class veiculo1(ABC):
+class veiculo1(ABC):  # Classe abstrata que chamamos de produto
     @abstractmethod
-    def buscar_cliente1(self) -> None: pass
+    def buscar_cliente1(self) -> None: pass  # Método abstrato
 
 
-class CarroLuxo1(veiculo1):
+class CarroLuxo1(veiculo1):  # Classes herdeiras do produto que complementam também os seu método abstrato
     def buscar_cliente1(self) -> None:
         print('Carro de luxo está buscando o cliente...')
 
@@ -31,19 +31,24 @@ class MotoLuxo1(veiculo1):
         print('Moto de luxo está buscando o cliente...')
 
 
+# A diferença com o padrão da aula anterior está em tornar a fábrica de objetos abstrata também, para que dessa forma não
+# ficarmos presos no método da fábrica e sim termos classes que irão compor o método abstrato da fábrica da forma que for
+# necessário, com os objetos que forem necessários apenas para aquela classe
 class VeiculoFactory1(ABC):
     def __init__(self, tipo):
         self.carro = self.get_carro1(tipo)
 
     @staticmethod
     @abstractmethod
-    def get_carro1(tipo: str) -> veiculo1:  # type: ignore
+    def get_carro1(tipo: str) -> veiculo1:  # type: ignore  # Método abstrato que irá receber as fábricas seguintes
         ...
 
     def buscar_cliente1(self):
         self.carro.buscar_cliente1()
 
 
+# Classe referente a primeira filial, os objetos criado por ela são especificos, pois criam apartir do método abstrado da classe
+# pai
 class Filial1Factory(VeiculoFactory1):
     @staticmethod
     def get_carro1(tipo: str) -> veiculo1:  # type: ignore
@@ -58,6 +63,8 @@ class Filial1Factory(VeiculoFactory1):
         assert 0, 'Veículo não existe'
 
 
+# A classe seguinte também é uma factory e tem seus objetos diferentes da anterior porém parte do mesmo método abstrato e do mesmo
+# pai
 class Filial2Factory(VeiculoFactory1):
     @staticmethod
     def get_carro1(tipo: str) -> veiculo1:  # type: ignore
