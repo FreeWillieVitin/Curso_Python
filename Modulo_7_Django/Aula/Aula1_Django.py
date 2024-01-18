b680e03deb8ef0729b414a944587f191ee9fae2a
Victor <vhjurczyszyn@gmail.com>
2024-01-17 21:56:47 -0300

    Documentação django e breve explicação

---
+++ Modulo_7_Django/Aula/Aula1_Django.py
@@ -0,0 +1,6 @@
+"""
+Documentação Django: https://docs.djangoproject.com/en/5.0/
+
+O que é Django?
+Django é um web framework usado para desenvolvimentos de aplicações web, aplicações mobile e etc.
+"""
